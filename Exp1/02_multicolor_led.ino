7dcc359e759b66158101155c144d35a269762473
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-01-03 16:32:48 +0000

    experiment 1

---
+++ Exp1/02_multicolor_led.ino
@@ -0,0 +1,31 @@
+int red_pin=10;
+int green_pin=1;
+int blue_pin=4;
+
+void setup() {
+  pinMode(red_pin,OUTPUT);
+  pinMode(green_pin,OUTPUT);
+  pinMode(blue_pin,OUTPUT);
+}
+
+void loop() {
+  RGBColor(255,0,0);
+  delay(500);
+  RGBColor(0,255,0);
+  delay(500);
+  RGBColor(0,0,255);
+  delay(500);
+  RGBColor(255,255,255);
+  delay(500);
+  RGBColor(255,165,0);
+  delay(500);
+  RGBColor(156,34,93);
+  delay(500);
+}
+
+void RGBColor(int red,int green,int blue)
+{
+  analogWrite(red_pin,red);
+  analogWrite(green_pin,green);
+  analogWrite(blue_pin,blue);
+}
